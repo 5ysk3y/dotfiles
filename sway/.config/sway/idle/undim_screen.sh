4a719e9db797d1ddfba8e7e285cdcfadf976a5fd
5ysk3y <5ysk3y@pm.me>
2021-06-21 17:49:35 +0100

    Updated the undim_screen.sh idle script Cleaned that file up properly as it contained some work arounds needed prior to a complete system rebuild; these workarounds are no longer needed, so I've stripped it down.

--- sway/.config/sway/idle/undim_screen.sh
+++ sway/.config/sway/idle/undim_screen.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Function to check brightness
+
+function getBrightness {
+  ddcutil getvcp 0x10 -b $i | awk -F"current value =" {'print $2'} | awk {'print $1'} | sed 's/,//g'
+}
+
 BORKED=$(ddcutil detect | grep -i busy | grep -Eo 'i2c-[0-9]' | sort | uniq)
 
 if [[ $BORKED -eq 0  ]]
@@ -7,21 +13,18 @@ then
   for i in $(ddcutil detect | grep -E '/dev/i2c-[0-9]' | awk {'print $3'} | sed 's/\/dev\/i2c-//g')
   do
     ddcutil setvcp 0x10 75 -b $i &
-  done
-elif [[ $BORKED -gt 1 ]]
-then
-  notify-send "Monitor Issue" "More than one borked monitor detected"
-else
-  for i in $(ddcutil detect | grep -E '/dev/i2c-[0-9]' | awk {'print $3'} | grep -vi "^$BORKED\|addr" | sed 's/\/dev\/i2c-//g')
-  do
-    ddcutil setvcp 0x10 75 -b $i &
 
     # Check to see if brightness changed:
-    CUR=$(ddcutil getvcp 0x10 -b $i | awk -F"current value =" {'print $2'} | awk {'print $1'} | sed 's/,//g')
+    CUR=$(getBrightness)
     # If it isn't 75, then loop until it is:
     while [[ $CUR -ne 75 ]]
     do
         ddcutil setvcp 0x10 75 -b $i &
+        sleep 0.5
+        CUR=$(getBrightness)
     done
   done
+
+else
+  notify-send "Monitor Issue" "More than one borked monitor detected"
 fi
